e736020979eac231636cdcc11dd5ea100f6c95a2
Christiaan Rademan <christiaan.rademan@gmail.com>
2017-02-23 12:48:17 +0200

    Added sql_get function to API

--- tachyon/api/api.py
+++ tachyon/api/api.py
@@ -70,6 +70,116 @@ def parse_body(body, domain, domain_id, tenant, tenant_id):
     return json.dumps(obj)
 
 
+def sql_get(table, req, resp, id, where=None, where_values=None):
+    db = nfw.Mysql()
+
+    tenant_id = req.context.get('tenant_id')
+    domain_id = req.context.get('domain_id')
+    domain_admin = req.context.get('domain_admin')
+
+    header_start = req.headers.get('X-Pager-Start')
+    header_limit = req.headers.get('X-Pager-Limit')
+    header_order = req.headers.get('X-Order-By')
+    search = req.headers.get('X-Search')
+
+    tenant_field = table_has_col(table, 'tenant_id')
+    domain_field = table_has_col(table, 'domain_id')
+
+    sql_values = []
+    sql_where = []
+    if id is not None:
+        sql_where.append("id = %s")
+        sql_values.append(id)
+
+    if domain_id is not None:
+        if domain_field is True:
+            sql_where.append("domain_id = %s")
+            sql_values.append(domain_id)
+    else:
+        if domain_field is True:
+            raise nfw.HTTPForbidden("Access Forbidden", "Require domain!")
+
+    sql_search_where = []
+    if search is not None:
+        search = "%s%s" % (search,'%')
+        for field in data._declared_fields:
+            f = getattr(data, field)
+            if isinstance(f, nfw.model.Fields.Text):
+                sql_search_where.append("%s like %s" % (field, '%s'))
+                sql_values.append(search)
+            if isinstance(f, nfw.model.Fields.Integer):
+                sql_search_where.append("%s like %s" % (field, '%s'))
+                sql_values.append(int(search))
+        if len(sql_search_where) > 0:
+            sql_search_string = " or ".join(sql_search_where)
+            sql_where.append("( %s )" % (sql_search_string,))
+
+    if tenant_id is not None:
+        if tenant_field is True:
+            sql_where.append("tenant_id = %s")
+            sql_values.append(tenant_id)
+    else:
+        if tenant_field is True:
+            if domain_admin is False:
+                raise nfw.HTTPForbidden("Access Forbidden", "Not within tenant!")
+
+    if where is not None:
+        sql_where.append(where)
+    if where_values is not None:
+        sql_values.extend(where_values)
+
+    sql_where_string = " and ".join(sql_where)
+
+    sql_pager = ""
+    if header_start is not None and header_limit is not None:
+        start = int(header_start)
+        limit = int(header_limit)
+        sql_pager = "limit %s, %s" % (start, limit)
+
+    sql_order = ""
+    if header_order is not None:
+        orders = header_order.split(',')
+        formatted_orders = []
+        for order in orders:
+            regex = re.compile('[^a-zA-Z_]')
+            order_options = order.split(' ')
+            order_field = order_options[0]
+            order_field = regex.sub('', order_field)
+            if order_field not in data._declared_fields:
+                raise nfw.HTTPInvalidParam(order_field)
+            order_type = "asc"
+            if len(order_options) == 2:
+                order_type = order_options[1].lower()
+            if order_type != "asc" and order_type != "desc":
+                order_type = "asc"
+            formatted_order = "%s %s" % (order_field, order_type)
+            formatted_orders.append(formatted_order)
+        formatted_orders = ",".join(formatted_orders)
+        sql_order = "ORDER BY %s " % (formatted_orders,)
+
+    sql_query = "SELECT * FROM %s" % (table,)
+    sql_count = "SELECT count(id) as count FROM %s" % (table,)
+    if len(sql_where) > 0:
+        sql_query = "%s where %s" % (sql_query, sql_where_string)
+        sql_count = "%s where %s" % (sql_count, sql_where_string)
+    sql_query = "%s %s %s" % (sql_query, sql_order, sql_pager)
+
+    count_result = db.execute(sql_count, sql_values)
+    resp.headers['X-Total-Rows'] = count_result[0]['count']
+    resp.headers['X-Filtered-Rows'] = count_result[0]['count']
+
+    result = db.execute(sql_count, sql_values)
+    db.commit()
+
+    if id is not None:
+        if len(result) == 1:
+            return json.dumps(result[0])
+        else:
+            raise nfw.HTTPNotFound("Not Found", "Object not found")
+    else:
+        return json.dumps(result, indent=4)
+
+
 def get(model, req, resp, id, where=None, where_values=None):
     db = nfw.Mysql()
     data = model(db=db)
